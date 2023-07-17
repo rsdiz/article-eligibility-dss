698058969885b678fc535e84966df134719c73b6
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2023-07-17 19:37:26 +0700

    feat: add new function to execute ajax only

--- resources/views/shared/ajax.blade.php
+++ resources/views/shared/ajax.blade.php
@@ -15,7 +15,7 @@ function requestAjaxPost(opt, form, txt) {
 
         opt.element.text(txt.btnText);
         opt.element.attr("disabled", false);
-        
+
         Swal.fire({
             toast: true,
             position: 'top-end',
@@ -36,6 +36,65 @@ function requestAjaxPost(opt, form, txt) {
         opt.element.text(txt.btnText);
         opt.element.attr("disabled", false);
 
+        if(err.status == 422) {
+            if(err.responseJSON['data'] !== undefined) {
+                Swal.fire({
+                    toast: true,
+                    position: 'top-end',
+                    icon: 'error',
+                    title: err.responseJSON.data,
+                    showConfirmButton: false,
+                    timer: 4000,
+                    background: '#DC2626',
+                });
+            }else{
+                var errorMessage = '';
+                $.each(err.responseJSON['errors'], function (i, error) {
+                    errorMessage += `<li>${error[0]}</li>`
+                });
+
+                $('.error-message').removeClass("d-none");
+                $('.error-message ul').prepend(errorMessage);
+            }
+        }else{
+            var errorMessage = '<li>Terjadi kesalahan pada server</li>';
+            $('.error-message').removeClass("d-none");
+            $('.error-message ul').prepend(errorMessage);
+        }
+    });
+}
+
+function requestAjaxPostOnly(opt, form, txt) {
+    $.ajax({
+        headers: { 'X-CSRF-TOKEN': '{{ csrf_token() }}' },
+        url: opt.url,
+        mimeType: 'multipart/form-data',
+        method: opt.method,
+        dataType: 'json',
+        data: form,
+        cache: false,
+        processData: false,
+        contentType: false
+    }).done(function(msg) {
+        $('button[name="close-modal"]').trigger('click');
+
+        Swal.fire({
+            toast: true,
+            position: 'top-end',
+            icon: 'success',
+            title: txt.msgAlert,
+            showConfirmButton: false,
+            timer: 4000,
+            background: '#059669',
+        });
+
+        if((opt.type == 'category')
+            && opt.table != '') opt.table.ajax.reload();
+
+        $('.modal input').val('');
+        $('.modal textarea').val('');
+        $('.modal select').val('');
+    }).fail(function(err) {
         if(err.status == 422) {
             if(err.responseJSON['data'] !== undefined) {
                 Swal.fire({
